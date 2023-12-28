b2ae31259fc61222c032c6034e7b9870f15dbb93
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2023-12-28 20:54:09 +0100

    Refactor face detection test to use local image file

--- code/crawler/test_face_detector.py
+++ code/crawler/test_face_detector.py
@@ -1,23 +1,15 @@
 import unittest
-import numpy
 import cv2
-import requests
-from io import BytesIO
 from face_detector import FaceDetector
 
 class TestFaceDetector(unittest.TestCase):
     def test_face_detection(self):
-        # Example image URL with a face
-        image_url = "https://upload.wikimedia.org/wikipedia/commons/2/2a/Human_faces.jpg"
+        # Path to the downloaded image
+        image_path = "face_image.jpg"
         
-        # Fetch the image from the URL
-        response = requests.get(image_url)
-        self.assertEqual(response.status_code, 200, "Failed to fetch the image")
+        # Read the image from the local file
+        image_array = cv2.imread(image_path)
 
-        # Convert the image data to a NumPy array
-        image_data = BytesIO(response.content)
-        image_array = cv2.imdecode(numpy.frombuffer(image_data.read(), numpy.uint8), -1)
-        
         # Perform face detection
         face_detector = FaceDetector("/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml")
         faces = face_detector.detect_faces(image_array)
