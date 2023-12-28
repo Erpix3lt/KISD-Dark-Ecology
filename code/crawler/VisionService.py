baae564e5e6c53a4f9816b9cb05a2aa2a04f614d
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2023-12-28 20:25:17 +0100

    Add VisionService class and refactor code

--- code/crawler/VisionService.py
+++ code/crawler/VisionService.py
@@ -1,9 +1,24 @@
 from picamera2 import Picamera2, Preview
 import time
 picam2 = Picamera2()
-# camera_config = picam2.create_preview_configuration()
-# picam2.configure(camera_config)
-# picam2.start_preview(Preview.DRM)
 picam2.start()
 time.sleep(2)
 picam2.capture_file("test.jpg")
+
+class VisionService:
+
+    def __init__(self):
+        self.picam2 = Picamera2()
+
+    def start(self):
+        self.picam2.start()
+        time.sleep(2)
+
+    def stop(self):
+        self.picam2.stop()
+        self.picam2.close()
+
+    def capture(self, filename):
+        self.start()
+        self.picam2.capture_file(filename)
+        self.stop()
