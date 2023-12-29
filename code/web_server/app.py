5d056cd3f539dc4d96bb6bfa63eb49d7f17fa0be
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2023-12-29 17:29:26 +0100

    Refactor image loading and display logic in app.py and index.html

--- code/web_server/app.py
+++ code/web_server/app.py
@@ -1,38 +1,19 @@
-from flask import Flask, render_template, send_from_directory, request, jsonify
+from flask import Flask, render_template, send_from_directory
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Assuming images are in the '_images' folder
+    image_folder = '_images'
+    images = [img for img in os.listdir(image_folder) if img.endswith((".jpg", ".png"))]
+    image_paths = [img for img in images]
+    return render_template('index.html', image_paths=image_paths)
 
-@app.route('/get_images')
-def get_images():
-    try:
-        folder_path = request.args.get('folder', default='', type=str)
-        folder_path = os.path.normpath(folder_path)  # Normalize the path
-        image_folder = os.path.abspath(folder_path)
-
-        # Check if the folder exists
-        if not os.path.exists(image_folder) or not os.path.isdir(image_folder):
-            return jsonify({'error': 'Invalid folder path'})
-
-        images = [img for img in os.listdir(image_folder) if img.endswith((".jpg", ".png"))]
-        image_paths = [os.path.join(folder_path, img) for img in images]
-
-        return jsonify({'imagePaths': image_paths})
-    except Exception as e:
-        return jsonify({'error': str(e)})
-
-@app.route('/<path:folder>/<filename>')
-def get_image(folder, filename):
-    try:
-        # Construct an absolute path directly
-        folder_path = os.path.abspath(folder)
-        return send_from_directory(folder_path, filename)
-    except Exception as e:
-        return str(e), 404
+@app.route('/_images/<filename>')
+def get_image(filename):
+    return send_from_directory('_images', filename)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
