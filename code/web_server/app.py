531d5e94e1da18f61315212976ea2be5cc060226
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2023-12-29 17:18:22 +0100

    dynamic image folder

--- code/web_server/app.py
+++ code/web_server/app.py
@@ -1,19 +1,23 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request, jsonify
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Assuming images are in the '_images' folder
-    image_folder = '_images'
+    return render_template('index.html')
+
+@app.route('/get_images')
+def get_images():
+    folder_path = request.args.get('folder', default='', type=str)
+    image_folder = os.path.join(os.getcwd(), folder_path)
     images = [img for img in os.listdir(image_folder) if img.endswith((".jpg", ".png"))]
     image_paths = [img for img in images]
-    return render_template('index.html', image_paths=image_paths)
+    return jsonify({'imagePaths': image_paths})
 
-@app.route('/_images/<filename>')
-def get_image(filename):
-    return send_from_directory('_images', filename)
+@app.route('/<folder>/<filename>')
+def get_image(folder, filename):
+    return send_from_directory(folder, filename)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
