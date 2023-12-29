143f297b1a4ab394fcff8aa8eec481ae1a1847cf
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2023-12-29 17:06:41 +0100

    Add image serving functionality to web server

--- code/web_server/app.py
+++ code/web_server/app.py
@@ -1,15 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 import os
 
 app = Flask(__name__)
+image_folder = '_images'
 
 @app.route('/')
 def index():
-    # Assuming images are in the 'assets' folder
-    image_folder = '../cam_images'
-    images = [img for img in os.listdir(image_folder) if img.endswith(".jpg")]  # Adjust the file extension if needed
+    # Assuming images are in the 'cam_images' folder
+    images = [img for img in os.listdir(image_folder) if img.endswith(".jpg")]
     image_paths = [os.path.join(image_folder, img) for img in images]
     return render_template('index.html', image_paths=image_paths)
+
+@app.route('/_images/<path:filename>')
+def get_image(filename):
+    return send_from_directory(image_folder, filename)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
