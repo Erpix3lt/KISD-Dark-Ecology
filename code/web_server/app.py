8bd3787ebd622c2325fe4d6021a5d38b49beed79
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2023-12-29 17:20:33 +0100

    Refactor get_images and get_image functions

--- code/web_server/app.py
+++ code/web_server/app.py
@@ -9,15 +9,29 @@ def index():
 
 @app.route('/get_images')
 def get_images():
-    folder_path = request.args.get('folder', default='', type=str)
-    image_folder = os.path.join(os.getcwd(), folder_path)
-    images = [img for img in os.listdir(image_folder) if img.endswith((".jpg", ".png"))]
-    image_paths = [img for img in images]
-    return jsonify({'imagePaths': image_paths})
+    try:
+        folder_path = request.args.get('folder', default='', type=str)
+        folder_path = os.path.normpath(folder_path)  # Normalize the path
+        image_folder = os.path.join(os.getcwd(), folder_path)
+
+        # Check if the folder exists
+        if not os.path.exists(image_folder) or not os.path.isdir(image_folder):
+            return jsonify({'error': 'Invalid folder path'})
+
+        images = [img for img in os.listdir(image_folder) if img.endswith((".jpg", ".png"))]
+        image_paths = [os.path.join(folder_path, img) for img in images]
+
+        return jsonify({'imagePaths': image_paths})
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 @app.route('/<folder>/<filename>')
 def get_image(folder, filename):
-    return send_from_directory(folder, filename)
+    try:
+        folder_path = os.path.join(os.getcwd(), folder)
+        return send_from_directory(folder_path, filename)
+    except Exception as e:
+        return str(e), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
