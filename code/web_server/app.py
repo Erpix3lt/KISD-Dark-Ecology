d53cb7c36ee06c92be07e53ba20149e526e217e3
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2023-12-29 16:59:00 +0100

    Update image folder path in app.py

--- code/web_server/app.py
+++ code/web_server/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Assuming images are in the 'assets' folder
-    image_folder = 'assets'
+    image_folder = '../cam_images'
     images = [img for img in os.listdir(image_folder) if img.endswith(".jpg")]  # Adjust the file extension if needed
     image_paths = [os.path.join(image_folder, img) for img in images]
     return render_template('index.html', image_paths=image_paths)
