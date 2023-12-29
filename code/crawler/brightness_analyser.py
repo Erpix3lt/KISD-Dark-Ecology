c07eb629260805fa73c2627ff7519d99a6a77bb4
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2023-12-29 17:50:31 +0100

    updated analysed + tests

--- code/crawler/brightness_analyser.py
+++ code/crawler/brightness_analyser.py
@@ -4,9 +4,23 @@ import logging
 class BrightnessAnalyser:
 
     def process_image(self, image):
+        # Cover the upper half of the image with black color
+        height, width = image.shape[:2]
+        image[:height // 2, :] = [0, 0, 0]  # Set upper half to black
+
+        # Convert the image to grayscale
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(gray)
-        width = image.shape[1]
-        is_left = max_loc[0] < width / 2
+
+        # Find the brightest point in the lower half of the image
+        _, max_val, _, max_loc = cv2.minMaxLoc(gray[height // 2 :, :])
+
+        # Get the width of the lower half of the image
+        lower_half_width = width // 2
+
+        # Determine whether the brightest spot is in the lower-left or lower-right quarter
+        is_lower_left = max_loc[0] < lower_half_width
+
         logging.debug("Brightness: %f", max_val)
-        return is_left
+        logging.debug("Is in lower-left quarter: %r", is_lower_left)
+
+        return is_lower_left
--- code/crawler/tests/test_bightness_analyser.py
+++ code/crawler/tests/test_bightness_analyser.py
@@ -4,18 +4,16 @@ from brightness_analyser import BrightnessAnalyser
 
 class TestBrightnessAnalyser(unittest.TestCase):
 
-    def test_process_image(self):
-        # Load the example image (adjust the file path accordingly)
-        image = cv2.imread('assets/bright_spot_top_right.jpg')
-
-        # Instantiate the BrightnessAnalyser
+    def test_process_image_lower_left(self):
+        image = cv2.imread('assets/bright_spot_lower_left.jpg')
         analyser = BrightnessAnalyser()
-
-        # Call the process_image method
         result = analyser.process_image(image)
+        self.assertTrue(result)
 
-        # The example image has its brightest spot on the right side,
-        # so the result should be False
+    def test_process_image_lower_right(self):
+        image = cv2.imread('assets/bright_spot_lower_right.jpg')
+        analyser = BrightnessAnalyser()
+        result = analyser.process_image(image)
         self.assertFalse(result)
 
 if __name__ == '__main__':
