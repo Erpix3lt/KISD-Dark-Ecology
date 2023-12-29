6262a61cb43e0680ceb84c5f93fb9d97ec34cb81
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2023-12-29 17:13:21 +0100

    Refactor image handling in web server

--- code/web_server/app.py
+++ code/web_server/app.py
@@ -2,18 +2,18 @@ from flask import Flask, render_template, send_from_directory
 import os
 
 app = Flask(__name__)
-image_folder = '_images'
 
 @app.route('/')
 def index():
-    # Assuming images are in the 'cam_images' folder
-    images = [img for img in os.listdir(image_folder) if img.endswith(".jpg")]
-    image_paths = [os.path.join(image_folder, img) for img in images]
+    # Assuming images are in the '_images' folder
+    image_folder = '_images'
+    images = [img for img in os.listdir(image_folder) if img.endswith((".jpg", ".png"))]
+    image_paths = [img for img in images]
     return render_template('index.html', image_paths=image_paths)
 
-@app.route('/_images/<path:filename>')
+@app.route('/_images/<filename>')
 def get_image(filename):
-    return send_from_directory(image_folder, filename)
+    return send_from_directory('_images', filename)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
