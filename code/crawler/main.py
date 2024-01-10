f45ef64b36813ad2c0311c7addd85fa83f350a25
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2024-01-10 12:45:54 +0100

    Add servo functionality to crawler code

--- code/crawler/main.py
+++ code/crawler/main.py
@@ -1,25 +1,34 @@
 from vision_service import VisionService
 from brightness_analyser import BrightnessAnalyser
+from servo_service import ServoService
 import time
 
 if __name__ == "__main__":
-    vs = VisionService()
+    vision_service = VisionService()
     brightness_analyser = BrightnessAnalyser()
+    servo_service = ServoService()
 
-    vs.start()
+    vision_service.start()
 
     try:
         while True:
-            image = vs.capture_array()
+            image = vision_service.capture_array()
             is_left = brightness_analyser.process_image(image)
             
             if is_left:
-                print("Left")
+                print("Bright spot is on the left.")
+                print("Now moving towards the left.")
+                servo_service.move_left()
+                print("Finished moving towards the left.")
             else:
-                print("Right")
+                print("Bright spot is on the right.")
+                print("Now moving towards the right.")
+                servo_service.move_right()
+                print("Finished moving towards the right.")
 
-            time.sleep(2) 
+            # Implement scanning for the bright spot on the current position here. Should it exveed a certain treshold, stop the robot for some time.
     except KeyboardInterrupt:
         pass
     finally:
-        vs.stop()
+        vision_service.stop()
+        servo_service.stop()
--- code/crawler/servo_service.py
+++ code/crawler/servo_service.py
@@ -1,57 +1,86 @@
 import RPi.GPIO as GPIO
 import time
+import logging
 
 class ServoService:
 
-    def __init__(self):
+    def __init__(self, right_servo_pin = 26, left_servo_pin = 13, start_position = 7.5):
         # Set the GPIO mode to BCM
         GPIO.setmode(GPIO.BCM)
         # Define both servo pins as outputs
-        self.right_servo_pin = 26
-        self.left_servo_pin = 13
+        self.right_servo_pin = right_servo_pin
+        self.left_servo_pin = left_servo_pin
         GPIO.setup(self.right_servo_pin, GPIO.OUT)
         GPIO.setup(self.left_servo_pin, GPIO.OUT)
         # Create PWM objects with a frequency of 50 Hz
         self.right_pwm = GPIO.PWM(self.right_servo_pin, 50)
         self.left_pwm = GPIO.PWM(self.left_servo_pin, 50)
         # bring both pwm into a neutral position
-        self.right_pwm.start(7.5)
-        self.left_pwm.start(7.5)
+        self.right_pwm.start(start_position)
+        self.left_pwm.start(start_position)
+        logging.debug("Initialised ServoService with the following values: right_servo_pin: %d, left_servo_pin: %d", self.right_servo_pin, self.left_servo_pin)
 
-    def go_left(self, speed_difference = 10, duration = 1):
+    def go_left(self, additional_speed = 5, curve_steepness = 2,  duration = 1):
         # during the duration of 1 second move in the left direction
-        add_speed = speed_difference / 2
-        self.right_pwm.ChangeDutyCycle(7.5 + add_speed)
-        self.left_pwm.ChangeDutyCycle(7.5 - add_speed)
+        logging.debug("Moving left with additional_speed: %d, curve_steepness: %d and duration: %d", additional_speed, curve_steepness, duration)
+        self.right_pwm.ChangeDutyCycle(7.5 + additional_speed)
+        self.left_pwm.ChangeDutyCycle(7.5 + (additional_speed / curve_steepness))
         time.sleep(duration)
         # bring both pwm into a neutral position
+        logging.debug("Bringing both pwm into a neutral position")
         self.right_pwm.ChangeDutyCycle(7.5)
         self.left_pwm.ChangeDutyCycle(7.5)
 
-    def go_right(self, speed_difference = 10, duration = 1):
+    def go_right(self, additional_speed = 5, curve_steepness = 2, duration = 1):
         # during the duration of 1 second move in the right direction
-        add_speed = speed_difference / 2
-        self.right_pwm.ChangeDutyCycle(7.5 - add_speed)
-        self.left_pwm.ChangeDutyCycle(7.5 + add_speed)
+        logging.debug("Moving right with additional_speed: %d, curve_steepness: %d and duration: %d", additional_speed, curve_steepness, duration)
+        self.right_pwm.ChangeDutyCycle(7.5 + (additional_speed / curve_steepness))
+        self.left_pwm.ChangeDutyCycle(7.5 + additional_speed)
         time.sleep(duration)
         # bring both pwm into a neutral position
+        logging.debug("Bringing both pwm into a neutral position")
         self.right_pwm.ChangeDutyCycle(7.5)
         self.left_pwm.ChangeDutyCycle(7.5)
 
     def stop(self):
         # bring both pwm into a neutral position
+        logging.debug("Stopping, bringing both pwm into a neutral position")
         self.right_pwm.ChangeDutyCycle(7.5)
         self.left_pwm.ChangeDutyCycle(7.5)
 
-    def go_forward(self, speed_difference = 10, duration = 1):
+    def go_forward(self, additional_speed = 5, duration = 1):
         # bring both pwm into a neutral position
-        add_speed = speed_difference / 2
-        self.right_pwm.ChangeDutyCycle(7.5 + add_speed)
-        self.left_pwm.ChangeDutyCycle(7.5 + add_speed)
+        logging.debug("Moving forward with additional_speed: %d and duration: %d", additional_speed, duration)
+        self.right_pwm.ChangeDutyCycle(7.5 + additional_speed)
+        self.left_pwm.ChangeDutyCycle(7.5 + additional_speed)
         time.sleep(duration)
         # bring both pwm into a neutral position
+        logging.debug("Bringing both pwm into a neutral position")
         self.right_pwm.ChangeDutyCycle(7.5)
         self.left_pwm.ChangeDutyCycle(7.5)
+
+    def go_backward(self, additional_speed = 5, duration = 1):
+        # bring both pwm into a neutral position
+        logging.debug("Moving backward with additional_speed: %d and duration: %d", additional_speed, duration)
+        self.right_pwm.ChangeDutyCycle(7.5 - additional_speed)
+        self.left_pwm.ChangeDutyCycle(7.5 - additional_speed)
+        time.sleep(duration)
+        # bring both pwm into a neutral position
+        logging.debug("Bringing both pwm into a neutral position")
+        self.right_pwm.ChangeDutyCycle(7.5)
+        self.left_pwm.ChangeDutyCycle(7.5)
+
+    def rotate(self, additional_speed = 5, duration = 1):
+        #implementation of a rotation
+        logging.debug("Rotating with additional_speed: %d and duration: %d", additional_speed, duration)
+        self.right_pwm.ChangeDutyCycle(7.5 - additional_speed)
+        self.left_pwm.ChangeDutyCycle(7.5 + additional_speed)
+        time.sleep(duration)
+
+    def rotate_360(self):
+        #implementation of a 360 degree rotation
+        logging.debug("Rotating 360 degrees")
+        self.rotate(duration=10)
 
     def close(self):
         # Clean up GPIO on program exit
