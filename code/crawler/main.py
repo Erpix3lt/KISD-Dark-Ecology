0d89a8446c7cfe034e217d7c95d738f2c795f669
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2024-01-10 14:27:15 +0100

    Refactor servo movement functions

--- code/crawler/main.py
+++ code/crawler/main.py
@@ -18,12 +18,12 @@ if __name__ == "__main__":
             if is_left:
                 print("Bright spot is on the left.")
                 print("Now moving towards the left.")
-                servo_service.move_left()
+                servo_service.go_left()
                 print("Finished moving towards the left.")
             else:
                 print("Bright spot is on the right.")
                 print("Now moving towards the right.")
-                servo_service.move_right()
+                servo_service.go_right()
                 print("Finished moving towards the right.")
 
             # Implement scanning for the bright spot on the current position here. Should it exveed a certain treshold, stop the robot for some time.
