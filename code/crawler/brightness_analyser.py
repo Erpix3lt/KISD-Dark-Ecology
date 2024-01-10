8f7787fc797fa35e6baa5f84eba74530a7a42929
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2024-01-10 23:32:59 +0100

    Set upper half of image to black in BrightnessAnalyser

--- code/crawler/brightness_analyser.py
+++ code/crawler/brightness_analyser.py
@@ -7,6 +7,7 @@ class BrightnessAnalyser:
     
     def process_image(self, image):
         height, width = image.shape[:2]
+        #image[:height // 2, :, :] = 0  # Set upper half to black
         gray_lower_half = cv2.cvtColor(image[height // 2 :, :], cv2.COLOR_BGR2GRAY)
         _, max_val, _, max_loc = cv2.minMaxLoc(gray_lower_half)
         lower_half_width = width // 2
