0285edd78ee58c07e4b174c17c9a8d1969e88c6c
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2024-01-10 14:30:42 +0100

    Add command line argument to disable servo movement

--- code/crawler/main.py
+++ code/crawler/main.py
@@ -1,9 +1,17 @@
 from vision_service import VisionService
 from brightness_analyser import BrightnessAnalyser
 from servo_service import ServoService
+import argparse
 import time
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='Process images and control servos based on brightness analysis.')
+    parser.add_argument('--no_servo', action='store_true', help='Disable servo movement')
+    return parser.parse_args()
+
 if __name__ == "__main__":
+    args = parse_args()
+
     vision_service = VisionService()
     brightness_analyser = BrightnessAnalyser()
     servo_service = ServoService()
@@ -17,18 +25,21 @@ if __name__ == "__main__":
             
             if is_left:
                 print("Bright spot is on the left.")
-                print("Now moving towards the left.")
-                servo_service.go_left()
-                print("Finished moving towards the left.")
+                if not args.no_servo:
+                    print("Now moving towards the left.")
+                    servo_service.go_left()
+                    print("Finished moving towards the left.")
             else:
                 print("Bright spot is on the right.")
-                print("Now moving towards the right.")
-                servo_service.go_right()
-                print("Finished moving towards the right.")
+                if not args.no_servo:
+                    print("Now moving towards the right.")
+                    servo_service.go_right()
+                    print("Finished moving towards the right.")
 
-            # Implement scanning for the bright spot on the current position here. Should it exveed a certain treshold, stop the robot for some time.
+            # Implement scanning for the bright spot on the current position here. Should it exceed a certain threshold, stop the robot for some time.
     except KeyboardInterrupt:
         pass
     finally:
         vision_service.stop()
-        servo_service.stop()
+        if not args.no_servo:
+            servo_service.stop()
