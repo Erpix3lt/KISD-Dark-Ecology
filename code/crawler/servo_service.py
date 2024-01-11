d296bdcf2d43db97131abab7e44a14be34ca42af
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2024-01-11 15:01:35 +0100

    added test

--- code/crawler/servo_service.py
+++ code/crawler/servo_service.py
@@ -87,22 +87,3 @@ class ServoService:
         self.left_pwm.stop()
         GPIO.cleanup()
 
-servoService = ServoService()
-
-while True:
-    print("going forward")
-    servoService.go_forward(duration=5)
-    print("going backward")
-    servoService.go_backward(duration=5)
-    print("going left")
-    servoService.go_left(duration=5)
-    print("going right")
-    servoService.go_right(duration=5)
-    print("rotating")
-    servoService.rotate(duration=5)
-    print("stopping")
-    servoService.stop(duration=5)
-    print("rotating 360")
-    servoService.rotate_360()
-    
-
---
+++ code/crawler/tests/servo_service_test.py
@@ -0,0 +1,21 @@
+from servo_service import ServoService
+
+servoService = ServoService()
+
+while True:
+    print("going forward")
+    servoService.go_forward(duration=5)
+    print("going backward")
+    servoService.go_backward(duration=5)
+    print("going left")
+    servoService.go_left(duration=5)
+    print("going right")
+    servoService.go_right(duration=5)
+    print("rotating")
+    servoService.rotate(duration=5)
+    print("stopping")
+    servoService.stop(duration=5)
+    print("rotating 360")
+    servoService.rotate_360()
+    
+
