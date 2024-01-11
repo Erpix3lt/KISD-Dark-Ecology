9c6decb25d876263bb3838ec6174908d358d2217
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2024-01-11 12:58:05 +0100

    Refactor brightness analysis code

--- code/crawler/brightness_analyser.py
+++ code/crawler/brightness_analyser.py
@@ -7,8 +7,13 @@ class BrightnessAnalyser:
     
     def process_image(self, image):
         height, width = image.shape[:2]
-        #image[:height // 2, :, :] = 0  # Set upper half to black
+        # Set upper half to black
+        image[:height // 2, :, :] = 0  
+        # Convert to grayscale
         gray_lower_half = cv2.cvtColor(image[height // 2 :, :], cv2.COLOR_BGR2GRAY)
+        # Add gausian blur, to prevent noise mistakes
+        gray_lower_half = cv2.GaussianBlur(gray_lower_half, (5, 5), 0)
+        # Get max brightness value and location
         _, max_val, _, max_loc = cv2.minMaxLoc(gray_lower_half)
         lower_half_width = width // 2
         is_lower_left = max_loc[0] < lower_half_width
@@ -16,8 +21,8 @@ class BrightnessAnalyser:
         logging.debug("Is in lower-left quarter: %r", is_lower_left)
 
         if logging.getLogger().isEnabledFor(logging.DEBUG):
-            cv2.circle(image, (max_loc[0], max_loc[1] + height // 2), 10, (0, 0, 255), 2)
-            cv2.line(image, (lower_half_width, 0), (lower_half_width, height), (255, 0, 0), 2)
+            cv2.circle(gray_lower_half, (max_loc[0], max_loc[1] + height // 2), 10, (0, 0, 255), 2)
+            cv2.line(gray_lower_half, (lower_half_width, 0), (lower_half_width, height), (255, 0, 0), 2)
 
-        return is_lower_left, image
+        return is_lower_left, gray_lower_half
 
