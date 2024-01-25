f5f8c046b3bf0586887b3961cfb510b5db6709b4
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2024-01-25 14:59:08 +0100

    Add image grouping and sorting functionality

--- code/web_server/app.py
+++ code/web_server/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, send_from_directory
 import os
+import re
 
 app = Flask(__name__)
 
@@ -8,8 +9,22 @@ def index():
     # Assuming images are in the '_images' folder
     image_folder = '_images'
     images = [img for img in os.listdir(image_folder) if img.endswith((".jpg", ".png"))]
-    image_paths = [img for img in images]
-    return render_template('index.html', image_paths=image_paths)
+    
+    # Group images by tags
+    grouped_images = {}
+    for img in images:
+        match = re.match(r'(\w+)_(\d{14})_\d+\.jpg', img)
+        if match:
+            tag, timestamp = match.group(1), match.group(2)
+            if tag not in grouped_images:
+                grouped_images[tag] = []
+            grouped_images[tag].append({'filename': img, 'timestamp': timestamp})
+
+    # Sort images by timestamp within each group
+    for tag in grouped_images:
+        grouped_images[tag] = sorted(grouped_images[tag], key=lambda x: x['timestamp'], reverse=True)
+
+    return render_template('index.html', grouped_images=grouped_images)
 
 @app.route('/_images/<filename>')
 def get_image(filename):
