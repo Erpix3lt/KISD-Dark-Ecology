4d35edfd663ec71a7180373aacd03b933f64e448
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2024-02-01 16:33:33 +0100

    Refactor servo rotation code

--- code/crawler/servo_service_test.py
+++ code/crawler/servo_service_test.py
@@ -2,7 +2,6 @@ from servo_service import ServoService
 
 servoService = ServoService()
 
-while True:
-    print("rotating for 3.5 seconds")
-    servoService.rotate_left_servo_once()
-   
+
+print("rotating for 3.5 seconds")
+servoService.rotate_left_servo_once()
