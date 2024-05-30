ac37efaae8196b0a66517bb71e68b3023b6a7429
Maximilian Schmalenbach <m.schmalenbach@sprinteins.com>
2024-05-30 13:01:13 +0200

    added object detection + new api requests + updated client

--- code/crawler/client.py
+++ code/crawler/client.py
@@ -1,19 +1,28 @@
 import requests
 from dotenv import load_dotenv
 import os
+from vision_service import VisionService
 
 class Client:
     def __init__(self):
-        load_dotenv()  
-        self.server_ip = os.getenv('SERVER_IP')  
-        self.server_port = int(os.getenv('SERVER_PORT'))
-        self.url = f'http://{self.server_ip}:{self.server_port}'
+      load_dotenv()  
+      self.server_ip = os.getenv('SERVER_IP')  
+      self.server_port = int(os.getenv('SERVER_PORT'))
+      self.url = f'http://{self.server_ip}:{self.server_port}'
+      self.vision_service = VisionService()
         
     def is_healthy(self):
-        response = requests.get(self.url + '/is_healthy')
-        return response.json()
+      response = requests.get(self.url + '/is_healthy')
+      return response.json()
+      
+    def analyse_image(self):
+      image = self.vision_service.capture_array()
+      response = requests.post(self.url + '/analyse_image', image)
+      return response.json()
 
 if __name__ == '__main__':
     client = Client()
     result = client.is_healthy()  
+    print(result)
+    result = client.analyse_image()  
     print(result)
--- code/server/server.py
+++ code/server/server.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
+from detection_service import Detection_Service
 
 class Server:
     def __init__(self):
@@ -8,10 +9,21 @@ class Server:
         self.app = Flask(__name__)
         self.port = int(os.getenv('PORT', 5500))
         self.host = os.getenv('HOST', '0.0.0.0')
+        self.detection_service = Detection_Service()
         
         @self.app.route('/is_healthy', methods=['GET'])
         def is_healthy():
             return jsonify({'is_running': True}), 200
+        
+        @self.app.route('/analyse_image', methods=['POST'])
+        def analyse_image():
+            data = request.get_json()
+            image = data.get('image')
+            try:
+                result, analysed_image = self.detection_service.analyse_image(image)
+                return jsonify({'result': result}), 200
+            except Exception as e:
+                return jsonify({'error': f'There was an error while analysing the image: {str(e)}'}), 400
         
         @self.app.route('/lead_me_to', methods=['POST'])
         def lead_me_to():
