3fd30ed3e84d71aa3015204d076186a7cf14b398
Maximilian Schmalenbach <m.schmalenbach@sprinteins.com>
2024-05-30 12:06:53 +0200

    refactored names

--- code/crawler/client.py
+++ code/crawler/client.py
@@ -6,15 +6,15 @@ class Client:
     def __init__(self):
         load_dotenv()  # Load environment variables from .env file
         self.server_ip = os.getenv('SERVER_IP')  # Retrieve SERVER_IP from environment variables
-        self.url = f'http://{self.server_ip}:5000/add'  # Construct the URL for the add endpoint
+        self.url = f'http://{self.server_ip}:5000'  # Construct the URL for the add endpoint
 
-    def send_numbers(self, a, b):
+    def post_add(self, a, b):
         payload = {'a': a, 'b': b}  # Data to send in the request
-        response = requests.post(self.url, json=payload)  # Send POST request
+        response = requests.post(self.url + '/add', json=payload)  # Send POST request
         return response.json()  # Return the server response
 
 # Example usage
 if __name__ == '__main__':
     client = Client()
-    result = client.send_numbers(3, 4)  # Send numbers 3 and 4 to the server
+    result = client.post_add(3, 4)  # Send numbers 3 and 4 to the server
     print(result)
