2a08ee020089f960c6b356ce9ec8ea45a8417ad5
Maximilian Schmalenbach <m.schmalenbach@sprinteins.com>
2024-05-30 14:31:02 +0200

    closed prev image

--- code/server/logger.py
+++ code/server/logger.py
@@ -5,15 +5,17 @@ from PIL import Image, ImageDraw
 
 class Logger:
     def __init__(self):
-      pass
+        pass
 
-    def log_analysed_image(self, image: Image.Image, add_timestamp = True):
-      if add_timestamp:
-        timestamp = time.strftime('%Y-%m-%d_%H-%M-%S')
-        draw = ImageDraw.Draw(image)
-        draw.text((10, 10), timestamp, fill='white')
+    def log_analysed_image(self, image: Image.Image, add_timestamp=True):
+        webbrowser.open('about:blank')
 
-      with tempfile.NamedTemporaryFile(suffix='.jpg', delete=False) as temp_file:
-          image.save(temp_file, format='JPEG')
-          temp_file_path = temp_file.name
-          webbrowser.open('file://' + temp_file_path)
+        if add_timestamp:
+            timestamp = time.strftime('%Y-%m-%d_%H-%M-%S')
+            draw = ImageDraw.Draw(image)
+            draw.text((10, 10), timestamp, fill='white')
+
+        with tempfile.NamedTemporaryFile(suffix='.jpg', delete=False) as temp_file:
+            image.save(temp_file, format='JPEG')
+            temp_file_path = temp_file.name
+            webbrowser.open('file://' + temp_file_path)
