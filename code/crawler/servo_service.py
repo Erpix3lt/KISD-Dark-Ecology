5da782f0c81d666f7bff3108f2cbf11126d8e0cb
Maximilian Schmalenbach <m.schmalenbach@sprinteins.com>
2024-05-31 16:52:30 +0200

    gg

--- code/crawler/servo_service.py
+++ code/crawler/servo_service.py
@@ -78,7 +78,7 @@ class ServoService:
         time.sleep(duration)
         
     def go_only_right(self, duration = 1):
-        self.twentysix_pwm.ChangeDutyCycle(self.twentysix_center_position - 1.7)
+        self.twentysix_pwm.ChangeDutyCycle(self.twentysix_center_position - 1.8)
         time.sleep(duration)
         
     def go_only_left(self, duration = 1):
